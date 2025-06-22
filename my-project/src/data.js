dfc38dc4a67ec6315e8c8cac4bf8efb2638b4e61
sena <gulozseven@gmail.com>
2025-06-22 12:06:38 +0300

    began to write english data

---
+++ my-project/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    en: {
+        headerSection: {
+            greeting: "Hi!",
+            intro: "I'm Sena. I'm a full-stack developer. I can craft solid and scalable frontend products. Let's meet!",
+            profileImg: "profile-image.jpg",
+            socials: [
+                {
+                    logo: "logo-linkedln.png",
+                    alt_text: "linkedin",
+                    link: "https://www.linkedin.com/in/g%C3%BCl-sena-%C3%B6zseven-418619198/",
+                },
+                {
+                    logo: "logo-github.png",
+                    alt_text: "github",
+                    link: "https://github.com/sena-ozseven",
+                },
+            ],
+            currentHTML: "Currently <strong>Freelancing</strong> for <strong>UX, UI, and Web Design</strong> projects. </br> Invite me to join your team --> <link mailto={gulozseven@gmail.com}>gulozseven@gmail.com</link>"
+        },
+
+        skillsSection: {
+            title: "Skills",
+            skills: [
+                {
+                    name: "JAVASCRIPT",
+                    icon: "icon-js.png",
+                },
+                {
+                    name: "REACT",
+                    icon: "icon-react.png",
+                },
+                {
+                    name: "REDUX",
+                    icon: "icon-redux.png",
+                },
+                {
+                    name: "NODE",
+                    icon: "icon-node.png",
+                },
+                {
+                    name: "VS CODE",
+                    icon: "icon-VS.png",
+                },
+                {
+                    name: "FIGMA",
+                    icon: "icon-figma.png",
+                },
+            ],
+        },
+    },
+
+}
